85cb7bb3dcb1c945bbafb01a4a4901e564eff66d
Rayce <rkronenberg@ufl.edu>
2025-11-15 19:33:18 -0500

    sjdfhsjd

--- test_calculator.py
+++ test_calculator.py
@@ -19,15 +19,15 @@ class TestCalculator(unittest.TestCase):
 
     ######## Partner 1
     def test_multiply(self): # 3 assertions
-        self.assertEqual(multiply(5,5), 25)
-        self.assertEqual(multiply(100,6), 600)
-        self.assertEqual(multiply(100000,1),100000)
+        self.assertEqual(mul(5,5), 25)
+        self.assertEqual(mul(100,6), 600)
+        self.assertEqual(mul(100000,1),100000)
 
 
     def test_divide(self): # 3 assertions
-        self.assertEqual(divide(50,5),10)
-        self.assertEqual(divide(100000,1), 100000)
-        self.assertEqual(divide(5,5),1)
+        self.assertEqual(div(50,5),10)
+        self.assertEqual(div(100000,1), 100000)
+        self.assertEqual(div(5,5),1)
     # ##########################
 
     ######## Partner 2
@@ -35,20 +35,20 @@ class TestCalculator(unittest.TestCase):
     #     # call division function inside, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #div(0, 5)
-        self.assertRaises(ZeroDivisionError, divide, 5, 0)
+        self.assertRaises(ZeroDivisionError, div, 5, 0)
 
     def test_logarithm(self): # 3 assertions
-        self.assertEqual(log(2, 4), 2)
-        self.assertEqual(log(-2, -8), 3)
-        self.assertEqual(log(5, 5), 1)
+        self.assertEqual(logarithm(2, 4), 2)
+        self.assertEqual(logarithm(-2, -8), 3)
+        self.assertEqual(logarithm(5, 5), 1)
 
     def test_log_invalid_base(self): # 1 assertion
     #     # use same technique from test_divide_by_zero
-        self.assertRaises(ZeroDivisionError, log, 5, 0)
+        self.assertRaises(ZeroDivisionError, logarithm, 5, 0)
     
 
     def test_log_invalid_argument(self): # 1 assertion
-         self.assertRaises(log(-1,5),ValueError)
+         self.assertRaises(logarithm(-1,5),ValueError)
 
     def test_hypotenuse(self): # 3 assertions
         self.assertEqual(hypotenuse(4,3),5)
@@ -56,9 +56,9 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(hypotenuse(6,8),10)
 
     def test_sqrt(self): # 3 assertions
-        self.assertRaises(sqrt(0), ValueError)
-        self.assertEqual(sqrt(25),5)
-        self.assertEqual(sqrt(100),10)
+        self.assertRaises(square_root(0), ValueError)
+        self.assertEqual(square_root(25),5)
+        self.assertEqual(square_root(100),10)
     ##########################
 
 # Do not touch this
